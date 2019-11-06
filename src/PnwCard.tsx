b749b70422f7e03155fe0f25d439249caafd8c3e
Martin <info@martinchavezaguilar.com>
2019-11-06 07:01:48 -0800

    Complete Card style

--- src/PnwCard.tsx
+++ src/PnwCard.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import Card, {
   CardPrimaryContent,
-  CardMedia,
-  CardActions,
-  CardActionButtons,
-  CardActionIcons
+  CardMedia
 } from "@material/react-card";
+import {
+  Body2,
+  Headline6
+} from '@material/react-typography';
+import image from './logo512.png'
+import './PnwCard.scss';
 
 class PnwCard extends React.Component {
   render() {
     return (
       <Card>
         <CardPrimaryContent>
-          <h1>Header</h1>
-          <CardMedia square imageUrl='./logo512.png' />
+          <CardMedia
+            square
+            imageUrl={image}
+          >
+          </CardMedia>
+          <div className='adventures-card__text-label'>
+            Our Changing Planet
+          </div>
+          <Body2 className='adventures-card__secondary'>
+            Visit ten places on our planet that are undergoing the biggest changes today.
+          </Body2>
         </CardPrimaryContent>
-
-        <CardActions>
-          <CardActionButtons>
-            <button>Click Me</button>
-          </CardActionButtons>
-
-          <CardActionIcons>
-            <i>Click Me Too!</i>
-          </CardActionIcons>
-        </CardActions>
       </Card>
     )
   }
