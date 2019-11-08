29da9f4bad3b5d85e5ee72ff0ce00927050b68d9
Martin <info@martinchavezaguilar.com>
2019-11-08 08:51:30 -0800

    Add image list, add real pictures

--- src/PnwAdventure.tsx
+++ src/PnwAdventure.tsx
@@ -6,6 +6,7 @@ import {
   Body2
 } from '@material/react-typography';
 import { Cell, Grid, Row } from '@material/react-layout-grid';
+import PnwImageList from './PnwImageList';
 
 type PnwAdventureProps = {
   adventure: Adventure
@@ -22,29 +23,32 @@ class PnwAdventure extends React.Component<PnwAdventureProps> {
 
   render() {
     return (
-      <Grid>
-        <Row>
-          <Cell></Cell>
-          <Cell>
-            <Headline3>{this.adventure.name}</Headline3>
-          </Cell>
-          <Cell></Cell>
-        </Row>
-        <Row>
-          <Cell></Cell>
-          <Cell>
-            <Body1>{this.adventure.smallDescription}</Body1>
-          </Cell>
-          <Cell></Cell>
-        </Row>
-        <Row>
-          <Cell></Cell>
-          <Cell>
-            <Body2>{this.adventure.description}</Body2>
-          </Cell>
-          <Cell></Cell>
-        </Row>
-      </Grid>
+      <>
+        <Grid>
+          <Row>
+            <Cell></Cell>
+            <Cell>
+              <Headline3>{this.adventure.name}</Headline3>
+            </Cell>
+            <Cell></Cell>
+          </Row>
+          <Row>
+            <Cell></Cell>
+            <Cell>
+              <Body1>{this.adventure.smallDescription}</Body1>
+            </Cell>
+            <Cell></Cell>
+          </Row>
+          <Row>
+            <Cell></Cell>
+            <Cell>
+              <Body2>{this.adventure.description}</Body2>
+            </Cell>
+            <Cell></Cell>
+          </Row>
+        </Grid>
+        <PnwImageList adventure={this.adventure}></PnwImageList>
+      </>
     )
   }
 }
---
+++ src/PnwImageList.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Adventure from './Adventure'
+
+type PnwImageListProps = {
+  adventure: Adventure
+}
+
+class PnwImageList extends React.Component<PnwImageListProps> {
+
+  adventure: Adventure;
+
+  constructor(props: PnwImageListProps) {
+    super(props);
+    this.adventure = props.adventure;
+  }
+
+  render() {
+    return (
+      <ul className="mdc-image-list mdc-image-list--masonry my-masonry-image-list">
+        <li className="mdc-image-list__item">
+          <img className="mdc-image-list__image" src={require('./media/' + this.adventure.imageUrl)} />
+        </li>
+      </ul>
+    )
+  }
+}
+
+export default PnwImageList;
