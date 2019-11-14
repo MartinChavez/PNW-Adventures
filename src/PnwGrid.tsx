7256a56ad87e031682bd3ce9977f262e31b370bc
Martin <info@martinchavezaguilar.com>
2019-11-14 08:01:26 -0800

    Extract function to Cell

--- src/PnwGrid.tsx
+++ src/PnwGrid.tsx
@@ -23,11 +23,22 @@ class PnwGrid extends React.Component<PnwGridProps & RouteComponentProps> {
     window.scrollTo(0, 0);
   }
 
+  getToCell(props: PnwGridProps & RouteComponentProps) {
+    return (adventure: Adventure) => {
+      return (
+        <Cell key={adventure.id} columns={4}>
+          <PnwCard {...this.props} adventure={adventure}>
+          </PnwCard>
+        </Cell>
+      )
+    }
+  }
+
   render() {
     return (
       <Grid>
         <Row>
-          {this.adventures.map(adventure => <Cell key={adventure.id} columns={4}><PnwCard {...this.props} adventure={adventure}></PnwCard></Cell>)}
+          {this.adventures.map(this.getToCell(this.props))}
         </Row>
       </Grid>
     )
