1598179c923be831e1acdcbf2346875743c0df0c
Martin Chavez <info@martinchavezaguilar.com>
2020-02-05 06:53:56 -0800

    Complete Copy functionality

--- src/ShareDialog.tsx
+++ src/ShareDialog.tsx
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
-import Dialog, {DialogTitle, DialogContent} from '@material/react-dialog';
-import TextField, {HelperText, Input} from '@material/react-text-field';
-import MaterialIcon from '@material/react-material-icon';
+import React, { Component } from 'react';
+import Dialog, { DialogTitle, DialogContent } from '@material/react-dialog';
+import TextField, { Input } from '@material/react-text-field';
+import Button from '@material/react-button';
+import copyTextToClipboard from './copyTextToClipboard'
 
 type ShareDialogProps = {
-    isOpen: boolean;
-    closeDialog: () => void;
+  isOpen: boolean;
+  closeDialog: () => void;
 }
 
 class ShareDialog extends Component<ShareDialogProps> {
@@ -13,24 +14,29 @@ class ShareDialog extends Component<ShareDialogProps> {
   getInputValue = () => {
     return window.location.href;
   }
-
-    render() {
-      return (
-        <Dialog 
-          open={this.props.isOpen} 
-          onClose={this.props.closeDialog}>
-          <DialogTitle>Share</DialogTitle>
-          <DialogContent>
-          <TextField
-          onTrailingIconSelect={() => this.setState({value: ''})}
-          trailingIcon={<MaterialIcon role="button" icon="delete"/>}
-        ><Input
-           value={this.getInputValue()} readOnly/>
-        </TextField>
-          </DialogContent>
-        </Dialog>
-      );
-    }
+  
+  copyClick = () => {
+    copyTextToClipboard(window.location.href);
   }
+
+  render() {
+    return (
+      <Dialog
+        open={this.props.isOpen}
+        onClose={this.props.closeDialog}>
+        <DialogTitle>Share</DialogTitle>
+        <DialogContent>
+          <TextField
+          ><Input
+              value={this.getInputValue()} readOnly />
+          </TextField>
+          <Button onClick={this.copyClick}>
+            Copy
+      </Button>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+}
 
 export default ShareDialog;
