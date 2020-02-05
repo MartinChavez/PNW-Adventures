dda1627f20676fdd517bbb8faac00302d517376f
Martin Chavez <info@martinchavezaguilar.com>
2020-02-05 07:20:03 -0800

    clean code from helper function

--- src/copyTextToClipboard.ts
+++ src/copyTextToClipboard.ts
@@ -44,15 +44,7 @@ function copyTextToClipboard(text: string) {
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
-  
-    try {
-      var successful = document.execCommand('copy');
-      var msg = successful ? 'successful' : 'unsuccessful';
-      console.log('Copying text command was ' + msg);
-    } catch (err) {
-      console.log('Oops, unable to copy');
-    }
-  
+    document.execCommand('copy');
     document.body.removeChild(textArea);
   }
 
