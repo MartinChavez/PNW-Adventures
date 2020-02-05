951fd7cb8a3037e25df3c05512e7123bb1879214
Martin Chavez <info@martinchavezaguilar.com>
2020-02-05 07:10:28 -0800

    Complete copy to clipboard functionality

--- src/ShareDialog.tsx
+++ src/ShareDialog.tsx
@@ -3,38 +3,62 @@ import Dialog, { DialogTitle, DialogContent } from '@material/react-dialog';
 import TextField, { Input } from '@material/react-text-field';
 import Button from '@material/react-button';
 import copyTextToClipboard from './copyTextToClipboard'
+import { Snackbar } from '@material/react-snackbar';
 
 type ShareDialogProps = {
   isOpen: boolean;
   closeDialog: () => void;
 }
 
-class ShareDialog extends Component<ShareDialogProps> {
+type ShareDialogState = {
+  snackBarIsOpen: boolean
+}
+
+class ShareDialog extends Component<ShareDialogProps, ShareDialogState> {
+
+  constructor(props: ShareDialogProps) {
+    super(props);
+    this.state = {
+      snackBarIsOpen: false
+    }
+  }
 
   getInputValue = () => {
     return window.location.href;
   }
-  
+
   copyClick = () => {
+    this.setState({
+      snackBarIsOpen: true
+    });
     copyTextToClipboard(window.location.href);
+  }
+
+  onSnackBarClose = () => {
+    this.setState({
+      snackBarIsOpen: false
+    });
   }
 
   render() {
     return (
-      <Dialog
-        open={this.props.isOpen}
-        onClose={this.props.closeDialog}>
-        <DialogTitle>Share</DialogTitle>
-        <DialogContent>
-          <TextField
-          ><Input
-              value={this.getInputValue()} readOnly />
-          </TextField>
-          <Button onClick={this.copyClick}>
-            Copy
-      </Button>
-        </DialogContent>
-      </Dialog>
+      <>
+        <Dialog
+          open={this.props.isOpen}
+          onClose={this.props.closeDialog}>
+          <DialogTitle>Share</DialogTitle>
+          <DialogContent>
+            <TextField
+            ><Input
+                value={this.getInputValue()} readOnly />
+            </TextField>
+            <Button onClick={this.copyClick}>
+              Copy
+             </Button>
+          </DialogContent>
+        </Dialog>
+        {this.state.snackBarIsOpen && <Snackbar message="URL copied to clipboard" onClose={this.onSnackBarClose} />}
+      </>
     );
   }
 }
