22ad24db5b423466d83e1891fdd23e216c2c536d
Martin Chavez <info@martinchavezaguilar.com>
2020-03-21 11:17:51 -0700

    Add main logo

--- src/PnwTopAppBar.tsx
+++ src/PnwTopAppBar.tsx
@@ -5,7 +5,7 @@ import TopAppBar, {
   TopAppBarSection,
   TopAppBarTitle,
 } from '@material/react-top-app-bar';
-import MaterialIcon from '@material/react-material-icon';
+import pnwLogo from './media/icons/logo.png'; //
 
 class PnwTopAppBar extends React.Component {
   render() {
@@ -13,10 +13,12 @@ class PnwTopAppBar extends React.Component {
       <TopAppBar>
         <TopAppBarRow>
           <TopAppBarSection align='start'>
-            <TopAppBarIcon navIcon tabIndex={0}>
-              <MaterialIcon hasRipple icon='menu' />
+            <TopAppBarIcon navIcon>
+              <img src={pnwLogo} alt="PNW Logo" />
             </TopAppBarIcon>
-            <TopAppBarTitle>Pacific Northwest Adventures</TopAppBarTitle>
+            <TopAppBarTitle>
+              Pacific Northwest Adventures
+              </TopAppBarTitle>
           </TopAppBarSection>
         </TopAppBarRow>
       </TopAppBar>
