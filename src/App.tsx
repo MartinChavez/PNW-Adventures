fa4e7e6a576085dd58f6572537ac8b87f00cc540
Martin Chavez <info@martinchavezaguilar.com>
2020-04-04 09:56:33 -0700

    Add HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,91 @@
-import React from 'react';
-import PnwTopAppBar from './PnwTopAppBar';
-import PnwTopAppBarAdventure from './PnwTopAppBarAdventure';
-import PnwGrid from './PnwGrid';
-import PnwAdventure from './PnwAdventure';
-import ShareDialog from './ShareDialog';
-import {
-  TopAppBarFixedAdjust
-} from '@material/react-top-app-bar';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
-import './App.scss';
-import adventures from './data/adventures.json';
-import Adventure, { fromAdventureJson } from './Adventure'
+import React from "react";
+import PnwTopAppBar from "./PnwTopAppBar";
+import PnwTopAppBarAdventure from "./PnwTopAppBarAdventure";
+import PnwGrid from "./PnwGrid";
+import PnwAdventure from "./PnwAdventure";
+import ShareDialog from "./ShareDialog";
+import { TopAppBarFixedAdjust } from "@material/react-top-app-bar";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import "./App.scss";
+import adventures from "./data/adventures.json";
+import Adventure, { fromAdventureJson } from "./Adventure";
 
-type AppProps = {}
+type AppProps = {};
 type AppState = {
   shareDialogIsOpen: boolean;
-}
+};
 
 class App extends React.Component<AppProps, AppState> {
-
   adventures: Adventure[];
 
   constructor(props: AppProps) {
     super(props);
     this.adventures = adventures.map(fromAdventureJson);
     this.state = {
-      shareDialogIsOpen: false
-    }
+      shareDialogIsOpen: false,
+    };
   }
 
   getAdventure(adventurePath: string): Adventure {
-    return this.adventures.filter(adventure => adventure.path === adventurePath)[0];
+    return this.adventures.filter(
+      (adventure) => adventure.path === adventurePath
+    )[0];
   }
 
-  closeDialog = () => this.setState({ shareDialogIsOpen: false })
-  openDialog = () => this.setState({ shareDialogIsOpen: true })
+  closeDialog = () => this.setState({ shareDialogIsOpen: false });
+  openDialog = () => this.setState({ shareDialogIsOpen: true });
 
   render() {
     return (
-      <Router>
+      <HashRouter>
         <Switch>
           <Route exact path="/">
-            <PnwTopAppBar>
-            </PnwTopAppBar>
+            <PnwTopAppBar></PnwTopAppBar>
           </Route>
-          <Route path="/adventures/:adventurePath" render={(props) =>
-            <PnwTopAppBarAdventure {...props} adventure={this.getAdventure(props.match.params.adventurePath)} openDialog={this.openDialog}  />} />
+          <Route
+            path="/adventures/:adventurePath"
+            render={(props) => (
+              <PnwTopAppBarAdventure
+                {...props}
+                adventure={this.getAdventure(props.match.params.adventurePath)}
+                openDialog={this.openDialog}
+              />
+            )}
+          />
         </Switch>
         <TopAppBarFixedAdjust>
           <Switch>
-            <Route exact path="/" render={(props) =>
-              <PnwGrid {...props} adventures={this.adventures} />} />
-            <Route path="/adventures/:adventurePath" render={(props) =>
-              <PnwAdventure {...props} adventure={this.getAdventure(props.match.params.adventurePath)} />} />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <PnwGrid {...props} adventures={this.adventures} />
+              )}
+            />
+            <Route
+              path="/adventures/:adventurePath"
+              render={(props) => (
+                <PnwAdventure
+                  {...props}
+                  adventure={this.getAdventure(
+                    props.match.params.adventurePath
+                  )}
+                />
+              )}
+            />
             <Route>
               <Redirect to="/" />
             </Route>
           </Switch>
         </TopAppBarFixedAdjust>
-        <ShareDialog isOpen={this.state.shareDialogIsOpen } closeDialog={this.closeDialog} > </ShareDialog>
-      </Router>
-    )
+        <ShareDialog
+          isOpen={this.state.shareDialogIsOpen}
+          closeDialog={this.closeDialog}
+        >
+          {" "}
+        </ShareDialog>
+      </HashRouter>
+    );
   }
 }
 
